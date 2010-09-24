554c92e6884cb2bbe8c7d64148dffb622113a823
Hugo Josefson <hugo@josefson.org>
2010-09-24 17:38:23 +0200

    Implemented SecretsTest.loginOrCreateMasterPassword().

--- secrets-for-android/secrets-for-android-test/src/net/tawacentral/roger/secrets/SecretsForAndroidRobotiumTest.java
+++ secrets-for-android/secrets-for-android-test/src/net/tawacentral/roger/secrets/SecretsForAndroidRobotiumTest.java
@@ -6,6 +6,7 @@ import junit.framework.Assert;
 
 public class SecretsForAndroidRobotiumTest extends ActivityInstrumentationTestCase2<LoginActivity> {
 
+	private static final String MASTER_PASSWORD = "Some password :)";
 	private Solo solo;
 
 	public SecretsForAndroidRobotiumTest() throws ClassNotFoundException {
@@ -20,6 +21,23 @@ public class SecretsForAndroidRobotiumTest extends ActivityInstrumentationTestCa
 	public void testLoginActivityAppears(){
 		solo.assertCurrentActivity("Login activity did not appear.", LoginActivity.class);
 		Assert.assertTrue(solo.searchText("Welcome to your Secrets"));
+	}
+
+	private void loginOrCreateMasterPassword(){
+		if (solo.searchText("Create a password to protect your secrets.")){
+			// Create password
+			solo.enterText(0, MASTER_PASSWORD);
+			solo.sendKey(Solo.ENTER);
+
+			Assert.assertTrue(solo.searchText("Validate your new password"));
+			solo.enterText(0, MASTER_PASSWORD);
+			solo.sendKey(Solo.ENTER);
+		}else {
+			// Login
+			Assert.assertFalse(solo.searchText("Validate your new password"));
+			solo.enterText(0, MASTER_PASSWORD);
+			solo.sendKey(Solo.ENTER);
+		}
 	}
 
 }
