2e225daab411b01c5f4a6b94d5fa76bfc14e0f4e
Hugo Josefson <hugo@josefson.org>
2010-09-28 15:27:20 +0200

    Implemented SRPlayerRobotiumTest.testFavouriteProgramme().

--- srplayer/srplayer-test/src/sr/player/test/SRPlayerRobotiumTest.java
+++ srplayer/srplayer-test/src/sr/player/test/SRPlayerRobotiumTest.java
@@ -1,6 +1,8 @@
 package sr.player.test;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.widget.ListView;
 import com.jayway.android.robotium.solo.Solo;
 import junit.framework.Assert;
 import sr.player.SRPlayer;
@@ -27,5 +29,19 @@ public class SRPlayerRobotiumTest extends ActivityInstrumentationTestCase2<SRPla
 		solo.clickOnText("Favoriter");
 		Assert.assertTrue(solo.searchText("Offline avsnitt"));
 	}
+
+    public void testFavouritesProgramme(){
+        solo.clickOnText("Program A-Ö");
+        Assert.assertTrue(solo.waitForText("Program A-Ö", 2, 90000, false));
+        solo.clickLongOnText("Barnradion i P4");
+        solo.clickOnText("Lägg till.*");
+
+        solo.clickOnText("Favoriter");
+        solo.clickOnText("1 favorit är tillagd");
+        Assert.assertTrue(solo.searchText("Favoriter - Program"));
+        Assert.assertTrue(solo.searchText("Barnradion i P4"));
+        solo.clickLongOnText("Barnradion i P4");
+        solo.clickOnText("Ta bort.*");
+    }
 
 }
